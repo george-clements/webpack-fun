5b7bc8b2aa3daa81f4c77696641902048c794f7e
George Clements <george@mohrstade.de>
2025-07-25 14:56:19 +0200

    Updated extension from require

--- index.js
+++ index.js
@@ -2,25 +2,7 @@ var window = {};
 var awin = require('./awin.js');
 var items = require('./items.json');
 var facebook = require('./facebook.js');
-require('./example_function');
-
-if (!window.fbq) {
-      !(function (f, b, e, v, n, t, s) {
-        if (f.fbq) {
-          return;
-        }
-        n = f.fbq = function () {
-          n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-        };
-        if (!f._fbq) {
-          f._fbq = n;
-        }
-        n.push = n;
-        n.loaded = !0;
-        n.version = "2.0";
-        n.queue = [];
-      })(window, '', "script", "//connect.facebook.net/en_US/fbevents.js");
-    }
+require('./example_function.js');
 
 console.log(JSON.stringify(facebook(items)));
 
