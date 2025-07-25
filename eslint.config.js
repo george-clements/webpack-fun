c137e2fcdbb8154e2ac11aacab5110c2c4da2ef4
Ronald Fortmann <github.com.surgery020@passmail.net>
2025-07-25 15:46:06 +0200

    fix: eslint rule - template literal restriction

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   rules: {
     "no-alert": "error",
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TemplateLiteral",
+        message:
+          "Template literals (backticks) are not allowed. Use string concatenation instead.",
+      },
+    ],
   },
 };
