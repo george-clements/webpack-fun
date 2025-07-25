1c4584b1bedd5d7b1ca737aa09bed6514f334f65
Ronald Fortmann <github.com.surgery020@passmail.net>
2025-07-25 14:57:11 +0200

    fix: Facebook initialization relocated

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 var window = {};
 
-var items = require("./items.json");
+var items = require("./items");
 
 var facebook = require("./facebook");
 
 facebook(items);
 
 require("./example_function");
-
-if (!window.fbq) {
-  !(function (f, b, e, v, n, t, s) {
-    if (f.fbq) {
-      return;
-    }
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) {
-      f._fbq = n;
-    }
-    n.push = n;
-    n.loaded = !0;
-    n.version = "2.0";
-    n.queue = [];
-  })(window, "", "script", "//connect.facebook.net/en_US/fbevents.js");
-}
 
 var awinProductData = require("./awin");
 awinProductData(items);
