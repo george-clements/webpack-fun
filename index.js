248d3ff845bae04f4e5472dda07e55a6b7f3f45b
George Clements <george@mohrstade.de>
2025-07-25 14:46:56 +0200

    Added awin and items.json

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 var window = {};
-
-var items = [
-    {
-        "item_name": "Glycolic Acid 7 %",
-        "item_id": "1220479",
-        "item_category": "",
-        "item_brand": "The Ordinary",
-        "item_main_category": "Gesichtswasser (120108)",
-        "item_product_classification": "",
-        "net_price": 10.25,
-        "gross_price": 12.2,
-        "quantity": "1",
-        "item_name_affiliate": "The Ordinary;Glycolic Acid 7 %",
-        "item_type": "PRODUCT"
-    },
-    {
-        "item_name": "Direct Acids",
-        "item_id": "015557",
-        "item_category": "",
-        "item_brand": "The Ordinary",
-        "item_main_category": "Gesichtspeeling (120107)",
-        "item_product_classification": "Pflege / Make-Up",
-        "net_price": 9.08,
-        "gross_price": 10.8,
-        "quantity": "1",
-        "item_name_affiliate": "The Ordinary;Direct Acids",
-        "item_type": "PRODUCT"
-    }
-];
+var awin = require('./awin.js');
+var items = require('./items.json');
 
 var facebook_items = items.reduce(function(acc, item){
     acc.push({
@@ -37,9 +10,8 @@ var facebook_items = items.reduce(function(acc, item){
     return acc
 }, []);
 
-
-
 console.log(facebook_items);
+require('./example_function');
 
 if (!window.fbq) {
       !(function (f, b, e, v, n, t, s) {
@@ -58,43 +30,6 @@ if (!window.fbq) {
         n.queue = [];
       })(window, '', "script", "//connect.facebook.net/en_US/fbevents.js");
     }
-
-var awin_item_ids = [];
-var awin_item_quantities = [];
-var awin_item_names = [];
-var awin_item_categories = [];
-var awin_item_prices = [];
-
- var sProductData = items.reduce(function (acc, item) {
-          awin_item_ids.push(item.item_id);
-          awin_item_quantities.push(item.quantity);
-          awin_item_names.push(item.item_name_affiliate);
-          awin_item_categories.push(item.item_main_category);
-          awin_item_prices.push(item.net_price);
-          return (
-            acc +
-            "AW:P|" +
-            '123456' +
-            "|" +
-            'test1234' +
-            "|" +
-            item.item_id +
-            "|" +
-            item.item_brand +
-            ";" +
-            item.item_name +
-            "|" +
-            item.net_price +
-            "|" +
-            item.quantity +
-            "|" +
-            "" +
-            "|" +
-            "gc10" +
-            "|" +
-            item.item_main_category +
-            "\r\n"
-          );
-        }, "");
-
-console.log(sProductData);
+  
+console.log(awin);
+awin(items);
