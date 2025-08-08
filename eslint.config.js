83d5ab8e2d3ac7553e84c05c31a338ac748e6f2d
George Clements <george@mohrstade.de>
2025-08-08 10:04:58 +0200

    Push latest version

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rules: {
+        "no-alert": "error",
+        "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TemplateLiteral",
+        message: "Template literals (backticks) are not allowed. Use string concatenation instead."
+      }
+    ]
+    }
+}
--- index.js
+++
@@ -1,9 +0,0 @@
-var window = {};
-var awin = require('./awin.js');
-var items = require('./items.json');
-var facebook = require('./facebook.js');
-require('./example_function.js');
-
-console.log(JSON.stringify(facebook(items)));
-
-awin(items);
